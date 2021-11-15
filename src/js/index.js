b8bbbb49910a724392dec54ff2cc1ea467bf0bd3
JJongBin <gghkdu2@gmail.com>
2021-11-16 02:43:51 +0900

    feat: #11 best layout and button-top

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// best slide
 const carousel = document.querySelector('.best');
 const carouselScreen = carousel.querySelector('.best-swiper-container');
 const numScreen = carouselScreen.childElementCount;
@@ -8,7 +9,7 @@ let nowPos = 0;
 const handlePrev = () => {
   const pos = 100 / numScreen;
   nowPos += pos;
-  if(nowPos >= 0) nowPos = -(100 - 100 / numScreen);
+  if(nowPos > 1) nowPos = -(100 - 100 / numScreen);
   carouselScreen.style.transform = `translateX(${nowPos}%)`;
 }
 const handleNext = () => {
@@ -18,7 +19,14 @@ const handleNext = () => {
   carouselScreen.style.transform = `translateX(${nowPos}%)`;
 }
 
-
 prevBtn.addEventListener('click', handlePrev);
 nextBtn.addEventListener('click', handleNext);
 
+
+// button-top
+const topButton = document.querySelector('.button-top');
+
+const handleTopButton = () => {
+  window.scrollTo({top:0, left:0, behavior:'smooth'});
+}
+topButton.addEventListener('click', handleTopButton);
