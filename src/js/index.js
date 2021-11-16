c242cffc49d6d2e7926e61ebe7c8e50766039cdb
JJongBin <gghkdu2@gmail.com>
2021-11-16 23:51:10 +0900

    fix: Add animation index.html
    - index.html

--- src/js/index.js
+++ src/js/index.js
@@ -53,18 +53,25 @@ try{
 
 // ---------------------------------
 // button-top
+const topButton = document.querySelector('.button-top');
 try{
-  const topButton = document.querySelector('.button-top');
-  
   const handleTopButton = () => {
     const viewPortHeight = document.documentElement.clientHeight;
-    // window.scrollTo({top:viewPortHeight, left:0, behavior:'smooth'});
     window.scrollTo({top:0, left:0, behavior:'smooth'});
   };
   topButton.addEventListener('click', handleTopButton);
 }catch(e){
   console.log(e);
 };
+
+window.addEventListener('scroll', function() {
+  scrollPos = document.documentElement.scrollTop;
+  if(scrollPos > 30){
+    topButton.style.opacity = 1;
+  }else{
+    topButton.style.opacity = 0;
+  }
+});
 
 
 // ---------------------------------
